a0db3953167f83bc6fb8ea529b7ad58965036ad6
ghana-bambale <ghanshyam.moscot@gmail.com>
2021-10-28 14:53:36 +0530

    dummy file changed

--- test/test_raw.py
+++ test/test_raw.py
@@ -1,9 +1,9 @@
 from selenium import webdriver
-
+from config.config import Env_Variables
 
 def test_raw():
-    driver = webdriver.Firefox(executable_path="/home/thebadcoder/TestWorkspace/drivers/geckodriver")
-    driver.get("https://github.com/django")
+    driver = webdriver.Firefox(executable_path=Env_Variables['DRIVER_BIN'])
+    driver.get(Env_Variables['BASE_URL'])
     page_title = driver.title
     assert page_title == 'Django · GitHub'
 
@@ -14,12 +14,13 @@ def test_raw():
     repositories_data_ui = {}
     for section in repositories_section_list:
         repo_name = section.find_element_by_xpath("following::h3/a").text
-        print(repo_name)
+        # print(repo_name)
         repo_description = section.find_element_by_xpath("following::p").text
-        print(repo_description)
+        # print(repo_description)
         repositories_data_ui[repo_name] = repo_description
 
-    driver.find_element_by_tag_name
+    driver.close()
 
     print(len(repositories_data_ui))
     print(repositories_data_ui)
+
