1689901a35c6f86c65fbc8bb05fe178de5a29999
ghana-bambale <ghanshyam.moscot@gmail.com>
2021-10-29 11:24:59 +0530

    variables changed and comments added

--- test/test_api.py
+++ test/test_api.py
@@ -15,27 +15,38 @@
 from lxml import html
 import requests
 def test_get():
-    page = requests.get('https://github.com/orgs/django/repositories')
+    BASE_URI = "https://github.com"
+    ENDPOINT = "/orgs/django/repositories"
+
+    URL = BASE_URI + ENDPOINT
+    print(URL)
+    page = requests.get(URL)
+    # page = requests.get("https://github.com/orgs/django/repositories")
     tree = html.fromstring(page.content)
-    #This will create a list of buyers:
-    first_repo = tree.xpath('(//*[@itemprop="owns"]/following::h3/a)[20]/text()')
-    print(first_repo)
-    buyers = tree.xpath('//*[@itemprop="owns"]/following::h3/a/text()')
-    # #This will create a list of prices
-    prices = tree.xpath('//*[@itemprop="owns"]/following::p/text()')
+
+    #This will create a list of keys which is repository names:    
+    repo_name_list = tree.xpath('//*[@itemprop="owns"]/following::h3/a/text()')
+    # #This will create a list of repo_description_list
+    repo_description_list = tree.xpath('//*[@itemprop="owns"]/following::p/text()')
+    # print('repo_name_list: ', repo_name_list)
+    # print('repo_description_list: ', repo_description_list)
+
+    #Dummy xpath test
     # title = tree.xpath("//head/title/text()")
+
+    #To store repository name and its description in a dict record
     api_resp_dict = {}
-    # print('Buyers: ', buyers)
-    # print('Prices: ', prices)
-    for iter_index in range (0,len(buyers)):
-        # epo_name_list_form = older_line.split()
-        # repo_name = " ".join(repo_name_list_form)
-        new_item_formatted = buyers[iter_index].split()
+    
+    for iter_index in range (0,len(repo_name_list)):
+        # format the tab, nextline multispaced string to a concrete one
+        new_item_formatted = repo_name_list[iter_index].split()
         repo_name = " ".join(new_item_formatted)
 
-        new_value_formatted = prices[iter_index].split()
+        # format the tab, nextline multispaced string to a concrete one
+        new_value_formatted = repo_description_list[iter_index].split()
         repo_description = " ".join(new_value_formatted)
+
+        # Store repository name and descrition against it for better maintenance
         api_resp_dict[repo_name] = repo_description
-    print(first_repo)
-    # print(api_resp_dict)
-    # print(api_resp_dict['channels_redis'])
+
+    print(api_resp_dict['channels_redis'])
