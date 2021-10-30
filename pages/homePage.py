04897659ad89b42eb3c999122425e43b8a0b29ce
ghana-bambale <ghanshyam.moscot@gmail.com>
2021-10-30 10:05:35 +0530

    changed to accept category to select from home

--- pages/homePage.py
+++ pages/homePage.py
@@ -8,24 +8,33 @@ from pages.initialize import initialize
 class homePage(initialize):
     
     #Locators
-    REPOSITORIES_LOCATOR = "//a[contains(@class,'UnderlineNav-item') and contains(@href,'repositories')]"    
+    REPOSITORIES_LOCATOR = "//a[contains(@class,'UnderlineNav-item') and contains(@href,'repositories')]"
     # Old locator for recerence
     # REPOSITORIES_SECTION_LIST_LOCATOR = "//li[@class='Box-row']"
     REPOSITORIES_SECTION_LIST_LOCATOR = "//*[@itemprop='owns']"
     REPOSITORIES_NAME_LOCATOR = "following::h3/a"
     REPOSITORIES_DESCRIPTION_LOCATOR = "following::p"
 
+    PACKAGES_LOCATOR = "//a[contains(@class,'UnderlineNav-item') and contains(@href,'repositories')]"
+
     
-    def __init__(self):
-        super().__init__()
+    def __init__(self,driver):
+        # super().__init__(driver)
+        self.driver = driver
         self.wait_for_element = WebDriverWait(self.driver,20)
 
     def load(self):
         self.driver.get(Env_Variables.BASE_URL)
     
-    def click_repositories(self):
-        repository_element = self.driver.find_element_by_xpath(self.REPOSITORIES_LOCATOR)
-        repository_element.click()
+    def select_category(self,category_param):
+        if "repositor" in category_param.lower():
+            set_category = self.REPOSITORIES_LOCATOR
+        elif 'package' in category_param.lower():
+            set_category = self.PACKAGES_LOCATOR
+        
+        category_element = self.driver.find_element_by_xpath(set_category)
+        category_element.click()
+
         self.driver.refresh()
     
     def find_repository_records(self):
