85b5e7e9bfd35f1dca7ea391c68e05b90e9f0676
ghana-bambale <ghanshyam.moscot@gmail.com>
2021-10-31 15:38:14 +0530

    markers added and function names changed

--- test/test_repositories.py
+++ test/test_repositories.py
@@ -11,7 +11,8 @@ from additionalLibraries.additional_features import Additional_Functionalities
 class Test_Repositories(BaseTest):
     
     ui_repo_dict = {}
-    def test_GetRepositoryInfo(self):
+    @pytest.mark.smoke
+    def test_get_repositories_info(self):
         self.homePageObj = homePage(self.driver)        
         self.homePageObj.select_category("repositories")
         Test_Repositories.ui_repo_dict = self.homePageObj.find_repository_records()
@@ -19,14 +20,17 @@ class Test_Repositories(BaseTest):
         # print(Test_Repositories.ui_repo_dict)
 
     api_repo_dict = {}
-    def test_GetRepositoriesAPI(self):
-        URL = Env_Variables.BASE_URI + Env_Variables.ENDPOINT
+    @pytest.mark.smoke
+    def test_get_repositories_api(self):
+        ENDPOINT = "/orgs/django/repositories"
+        URL = Env_Variables.BASE_URI + ENDPOINT
         print(URL)
         raw_response = requests.get(URL)
+        assert raw_response.status_code == 200
         Test_Repositories.api_repo_dict= Additional_Functionalities.convert_html_dict(raw_response.text)
         # print(Test_Repositories.api_repo_dict)        
 
-# @pytest.mark.skip
+@pytest.mark.smoke
 def test_ui_api_response_validation():
     ui_repo_dict = Test_Repositories.ui_repo_dict
     api_repo_dict = Test_Repositories.api_repo_dict
