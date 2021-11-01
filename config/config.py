5cf3f53f1653e1939b8e8240a8705cdde83fd43b
ghana-bambale <ghanshyam.moscot@gmail.com>
2021-11-01 17:18:04 +0530

    driver location changed to dynamic

--- config/config.py
+++ config/config.py
@@ -1,4 +1,4 @@
 class Env_Variables:
-    DRIVER_BIN = "/home/thebadcoder/TestWorkspace/drivers/geckodriver"
+    DRIVER_BIN = "/drivers/geckodriver"
     BASE_URL = "https://github.com/django"
     BASE_URI = "https://github.com"
