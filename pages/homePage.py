1537a48b7c8ea0ee8ecc30ff315b86a282534f3c
ghana-bambale <ghanshyam.moscot@gmail.com>
2021-11-03 11:36:59 +0530

    unnecesary code removed and comments added

--- pages/homePage.py
+++ pages/homePage.py
@@ -9,8 +9,6 @@ class homePage(BasePage):
     
     #Locators
     REPOSITORIES_LOCATOR = "//a[contains(@class,'UnderlineNav-item') and contains(@href,'repositories')]"
-    # Old locator for recerence
-    # REPOSITORIES_SECTION_LIST_LOCATOR = "//li[@class='Box-row']"
     REPOSITORIES_SECTION_LIST_LOCATOR = "//*[@itemprop='owns']"
     REPOSITORIES_NAME_LOCATOR = "following::h3/a"
     REPOSITORIES_DESCRIPTION_LOCATOR = "following::p"
@@ -21,7 +19,6 @@ class homePage(BasePage):
     def __init__(self,driver):
         super().__init__(driver)
         self.driver = driver
-        self.wait_for_element = WebDriverWait(self.driver,7)
 
     # Replaced by setup method
     # def load(self):
@@ -35,35 +32,38 @@ class homePage(BasePage):
         elif "project" in category_param.lower():
             set_category = self.PROJECTS_LOCATOR
         
-        # category_element = self.driver.find_element_by_xpath(set_category)
         self.click_element(set_category)
-        # category_element.click()
         self.driver.refresh()
     
     def find_repository_records(self):
-        # self.wait_for_element()
+        # Wait for the list of elements which has repository records
         self.wait_for_element.until(EC.presence_of_all_elements_located((By.XPATH,self.REPOSITORIES_SECTION_LIST_LOCATOR)))
         repositories_section_list = self.driver.find_elements_by_xpath(self.REPOSITORIES_SECTION_LIST_LOCATOR)
+        
         # dictionary to store repoository name,description pairs
         repositories_data_ui = {}
+
+        # For every record find name and description
         for record in repositories_section_list:  
-            repo_name = repo_description = ""          
-            try: #Get the repository name
-                repo_name = record.find_element_by_xpath("descendant::h3/a").text
-                # print(repo_name)
-            except Exception as e:
-                print("Repo name not found")
+
+            # Set the name and description to None
+            repo_name = repo_description = None          
+            
+            repo_name = record.find_element_by_xpath("descendant::h3/a").text
+            # print(repo_name)
             try: #Get the repository description
                 repo_description = record.find_element_by_xpath("descendant::p").text
                 # print(repo_description)
-            except Exception as e: # Exception handling
+            except Exception: # Exception handling
                 print("Repo description not fond")
 
-            
-            if repo_name and repo_description:
-                repositories_data_ui[repo_name] = repo_description
-            elif  repo_name and not repo_description:
-                repositories_data_ui[repo_name] = None
+            repositories_data_ui[repo_name]=repo_description
+
+            # OLD LOGIC to be deleted
+            # if repo_name and repo_description:
+            #     repositories_data_ui[repo_name] = repo_description
+            # elif  repo_name and not repo_description:
+            #     repositories_data_ui[repo_name] = None
         
         return repositories_data_ui
     
