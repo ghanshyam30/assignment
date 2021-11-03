bafc7df4d3a301b21bc7b8d177e8b2b6a679609c
ghana-bambale <ghanshyam.moscot@gmail.com>
2021-11-03 11:37:11 +0530

    unnecesary code removed and comments added

--- pages/projectsPage.py
+++ pages/projectsPage.py
@@ -10,7 +10,6 @@ class projectsPage(BasePage):
     def __init__(self,driver):
         super().__init__(driver)
         self.driver = driver
-        self.wait_for_element = WebDriverWait(self.driver,7)
         
     #Locators
     RELEASED_PROJECTS_LOCATOR = "//a[(contains(.,'Projects') and not(contains(@href,'beta'))) and @class='filter-item']"
@@ -18,33 +17,30 @@ class projectsPage(BasePage):
     PROJECTS_NAME_LOCATOR = "descendant::h4/a"
     PROJECTS_DETAILS_LOCATOR = "descendant::p"
 
+    # To chose the projects tab
     def choose_released_projects(self):
+        # Refresh the page because sometimes the elements dont reflect and not found
         self.driver.refresh()
+        # Navigate to released projects
         self.click_element(self.RELEASED_PROJECTS_LOCATOR)
 
+    # To find the project records
     def find_project_records(self):
-        # self.wait_for_element()
         self.wait_for_element.until(EC.presence_of_all_elements_located((By.XPATH,self.PROJECTS_SECTION_LIST_LOCATOR)))
         projects_section_list = self.driver.find_elements_by_xpath(self.PROJECTS_SECTION_LIST_LOCATOR)
+        
         # dictionary to store repoository name,description pairs
         projects_data_ui = {}
-        for record in projects_section_list:  
-            projects_name = projects_description = ""          
-            try: #Get the repository name
-                projects_name = record.find_element_by_xpath(self.PROJECTS_NAME_LOCATOR).text
-                # print(projects_name)
-            except Exception as e:
-                print("Project name not found")
-            try: #Get the repository description
+        for record in projects_section_list:
+            projects_name = projects_description = None
+            projects_name = record.find_element_by_xpath(self.PROJECTS_NAME_LOCATOR).text
+            
+            # Handle when project description is empty
+            try:
                 projects_description = record.find_element_by_xpath(self.PROJECTS_DETAILS_LOCATOR).text
-                # print(projects_description)
-            except Exception as e: # Exception handling
+            except Exception:
                 print("Project description not fond")
 
-            
-            if projects_name and projects_description:
-                projects_data_ui[projects_name] = projects_description
-            elif  projects_name and not projects_description:
-                projects_data_ui[projects_name] = "NA"
+            projects_data_ui[projects_name] = projects_description
         
         return projects_data_ui
