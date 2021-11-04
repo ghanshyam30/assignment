d137e4f205a9a1db0a219744d94f6512363b4a21
ghana-bambale <ghanshyam.moscot@gmail.com>
2021-11-04 16:20:34 +0530

    handled parametrization for many executions

--- test/conftest.py
+++ test/conftest.py
@@ -6,15 +6,17 @@ from selenium import webdriver
 from config.config import Env_Variables
 import os
 
-@pytest.fixture(scope="class")
+@pytest.fixture(params=Env_Variables.REPO_LIST,scope="class")
 def setup(request):
     # Initialize driver instance    
     current_path = os.getcwd()
     driver = webdriver.Firefox(executable_path=current_path + Env_Variables.DRIVER_BIN)
-
+    split_repo_param = request.param.split(" ")
     # Open env URL for UI test
-    driver.get(Env_Variables.BASE_URL)
+    driver.get(Env_Variables.BASE_URL+"/"+split_repo_param[0])
     request.cls.driver = driver
+    request.cls.repo_to_test = split_repo_param[0]
+    request.cls.user_type = split_repo_param[1]
 
     # Teardown
     yield
