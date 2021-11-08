af56a0e1d22a5c037afc03056cbbf3948afef9e5
ghana-bambale <ghanshyam.moscot@gmail.com>
2021-11-08 15:39:22 +0530

    config changed to accept only repo account names

--- config/config.py
+++ config/config.py
@@ -2,4 +2,5 @@ class Env_Variables:
     DRIVER_BIN = "/drivers/geckodriver"
     BASE_URL = "https://github.com"
     BASE_URI = "https://api.github.com"
-    REPO_LIST = ['django org', 'flask user', 'ghanshyam30 user']
+    # REPO_LIST = ['django org', 'flask user', 'ghanshyam30 user']
+    REPO_LIST = ['django', 'flask', 'ghanshyam30']
