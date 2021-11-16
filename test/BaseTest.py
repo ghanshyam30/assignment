ab443f61b3603cb19aba94a944bcce0f6eed58d2
ghana-bambale <ghanshyam.moscot@gmail.com>
2021-11-16 07:50:57 +0530

    documentation added

--- test/BaseTest.py
+++ test/BaseTest.py
@@ -1,5 +1,11 @@
 import pytest
-
+import logging
+'''
+Why do we have base test to use setup fixure?
+Reason - so that all test classes can inherit this class and so that for all the classes the setup and teardown will be same
+'''
 @pytest.mark.usefixtures("setup")
 class BaseTest:
-    pass
+    # placeholder method to show that we can put additional functionality required for the tests here
+    def myOwnFunction(self):
+        logging.info("This is a function from BaseTest class")
